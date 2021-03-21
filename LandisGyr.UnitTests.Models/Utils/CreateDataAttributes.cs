17b8eee4a57df1c9bcce3f3dea707c18a1958b85
CesarCamillo <cesar.a.a.camillo@gmail.com>
2021-03-21 19:07:44 -0300

    Seta utilitários para os testes unitários

---
+++ LandisGyr.UnitTests.Models/Utils/CreateDataAttributes.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+using AutoMapper;
+using LandisGyr.ConsoleApp;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LandisGyr.UnitTests.Models.Utils
+{
+    public class CreateDataAttributes : AutoDataAttribute
+    {
+        public CreateDataAttributes() : base(SetupLandisGyrFixture)
+        {
+
+        }
+
+        private static IFixture SetupLandisGyrFixture()
+        {
+            var fix = new Fixture();
+            fix.Customize(new AutoNSubstituteCustomization());
+            SetupCellContext(fix);
+            SetupAutoMapper(fix);
+            return fix;
+        }
+
+        private static void SetupAutoMapper(Fixture fix)
+        {
+            var autoMapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(typeof(Startup));
+            });
+            fix.Inject(autoMapperConfig);
+            fix.Inject(autoMapperConfig.CreateMapper());
+        }
+
+        private static void SetupCellContext(Fixture fix)
+        {
+            var dbOptions = new DbContextOptionsBuilder()
+                                        .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            fix.Inject(new LandisGyrContext(dbOptions.Options));
+        }
+    }
+}
