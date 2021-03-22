3ea1ef21c12918903f70ea863e6b76d4c06d76fd
CesarCamillo <cesar.a.a.camillo@gmail.com>
2021-03-21 21:10:43 -0300

    Adiciona testes unitários da Request de FindAllEndpoints

---
+++ LandisGyr.UnitTests.Models/Features/FindAllEndpointsTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using LandisGyr.ConsoleApp;
+using LandisGyr.ConsoleApp.Features;
+using LandisGyr.ConsoleApp.Models;
+using LandisGyr.UnitTests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LandisGyr.UnitTests.Features
+{
+    public class FindAllEndpointsTests
+    {
+        [Trait(TraitsConstants.Category.Name, TraitsConstants.Category.Values.Unit)]
+        public class ListAllEndpointsHandlerTests
+        {
+            [Theory]
+            [CreateDataAttributes]
+            [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Feature)]
+            public async Task Handle_NullRequest_ThrowsArgumentNull(FindAllEndpointsHandler subject)
+            {
+                // Arrange            
+
+                // Act
+                Func<Task> act = () => subject.Handle(null, default);
+
+                // Assert
+                await act
+                    .Should()
+                    .ThrowExactlyAsync<ArgumentNullException>();
+            }
+
+            [Theory]
+            [CreateDataAttributes]
+            [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Feature)]
+            public async Task Handle_EmptyContext_ReturnsEmptyList(FindAllEndpointsHandler subject)
+            {
+                // Arrange
+
+                // Act
+                var result = await subject.Handle(new FindAllEndpoints(), default);
+
+                // Assert
+                result
+                    .Should()
+                    .NotBeNull().And
+                    .BeEmpty();
+            }
+
+            [Theory]
+            [CreateDataAttributes]
+            [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Feature)]
+            public async Task Handle_ExistingContext_ReturnsList([Frozen] LandisGyrContext context,
+                IEnumerable<Endpoint> endpoints,
+                FindAllEndpointsHandler subject)
+            {
+                // Arrange
+                context.AddRange(endpoints);
+                await context.SaveChangesAsync();
+
+                // Act
+                var result = await subject.Handle(new FindAllEndpoints(), default);
+
+                // Assert
+                result
+                    .Should()
+                    .NotBeNull().And
+                    .HaveSameCount(context.Endpoints);
+            }
+        }
+    }
+}
